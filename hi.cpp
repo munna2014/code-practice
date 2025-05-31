2270327000c00a75e8c1885bff177899c1b56218
munna2014 <143425779+munna2014@users.noreply.github.com>
2025-05-31 22:27:50 +0600

    initial commit

---
+++ hi.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+  ll T;
+  cin>>T;
+  while(T--){
+    cout<<"Hi!"<<endl;
+  }
+  return 0;
+}
