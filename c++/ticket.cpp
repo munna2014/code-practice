36af8b5bee9bf6d65350c4a99273bcf7ff418bc9
munna2014 <143425779+munna2014@users.noreply.github.com>
2025-08-29 00:45:20 +0600

    leetcode

--- c++/ticket.cpp
+++ c++/ticket.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 #define ll long long int
 int main(){
-  ios_base::sync_with_stdio(false);cin.tie(NULL);
-
-  ll n, m;
-  cin >> n >> m;
+ios_base::sync_with_stdio(false);cin.tie(NULL);
+ll n, m;
+cin >> n >> m;
   multiset<ll> price_of_ticket;
   for (ll i = 0; i < n; i++)
   {
--- test/ticket.cpp
+++ test/ticket.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 #define ll long long int
 int main(){
-  ios_base::sync_with_stdio(false);cin.tie(NULL);
-   
-  
+ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+
   return 0;
 }
